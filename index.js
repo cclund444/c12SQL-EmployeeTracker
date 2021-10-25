1db4559bab46e00b01f700732f65e526d1b8026f
cclund444 <87826114+cclund444@users.noreply.github.com>
2021-10-24 22:59:28 -0600

    start of prompts for adding

--- index.js
+++ index.js
@@ -5,3 +5,79 @@
 // seed my data base -- look at activity and sql docs on how to seed tables  
 
 const db = require('./db')
+const inquirer = require('inquirer');
+
+// Add Prompts
+// Department
+inquirer
+    .prompt([
+
+        {
+            type: "input",
+            name: "title",
+            message: "What is the name of your department?",
+        },
+
+    ])
+
+// Role
+inquirer
+    .prompt([
+
+        {
+            type: "input",
+            name: "title",
+            message: "What is your job title?",
+        },
+
+        {
+            type: "input",
+            name: "amount",
+            message: "What is your salary within your role of that department?",
+        },
+
+        {
+            type: "input",
+            name: "department",
+            message: "What is the name of the department you're in?",
+        },
+
+    ])
+
+// Employee
+inquirer
+    .prompt([
+
+        {
+            type: "input",
+            name: "title",
+            message: "What is your first name?",
+        },
+
+        {
+            type: "input",
+            name: "title",
+            message: "What is your last name?",
+        },
+
+        {
+            type: "input",
+            name: "title",
+            message: "What is your Role in your job?",
+        },
+
+        {
+            type: "input",
+            name: "department",
+            message: "What is the name of the manager of your department?",
+        },
+
+    ])
+
+// Update Prompts 
+
+
+
+
+
+// View All Prompts
